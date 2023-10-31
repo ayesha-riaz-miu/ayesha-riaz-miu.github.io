0eefe618008ab0c8aee7331c69e5ff63f007b4e0
ayesha-riaz-miu <147187423+ayesha-riaz-miu@users.noreply.github.com>
2023-10-30 19:26:37 -0500

    Create Q4part1.js

---
+++ SD415/W1d1/Q4part1.js
@@ -0,0 +1,23 @@
+// 12345
+// 12345
+// 12345
+// 12345
+// 12345
+
+
+import PromptSync from "prompt-sync"; 
+const prompt = PromptSync();
+
+for(let i=1;i<6;i++){
+    //console.log(i)
+    let str="";
+    for(let j=1;j<6;j++)
+   
+    {
+       
+        str=str+j
+    }
+    console.log(str)
+   
+
+}
