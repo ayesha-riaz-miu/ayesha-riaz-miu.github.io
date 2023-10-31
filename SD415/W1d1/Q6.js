510736c8385e6f1a762d54a69126ef03cb90f8ce
ayesha-riaz-miu <147187423+ayesha-riaz-miu@users.noreply.github.com>
2023-10-30 19:28:40 -0500

    Create Q6.js

---
+++ SD415/W1d1/Q6.js
@@ -0,0 +1,28 @@
+// 6. Write a JavaScript program to prompt for an integer and compute the sum of all the digits. 
+// Use the / and % operators to find the digits.
+
+
+
+import PromptSync from "prompt-sync"; 
+const prompt = PromptSync();
+
+
+
+let number=[1,0,2]
+let sum=0;
+for(let i=0;i<number.length;i++)
+{
+    sum=sum+number[i]
+}
+console.log(sum)
+
+
+
+
+
+
+
+
+
+
+
