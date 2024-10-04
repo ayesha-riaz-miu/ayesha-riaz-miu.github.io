b032a5d0a714e74c416eb68cdcd63d91253d8319
Ayesha Riaz <ariaz@miu.edu>
2024-10-04 11:10:22 -0500

    new data

--- SD-545(React)/Final_Project/client/src/apis/axios.tsx
+++ SD-545(React)/Final_Project/client/src/apis/axios.tsx
@@ -10,14 +10,4 @@ const instance = axios.create({
   },
 });
 
-// axios.interceptors.request.use(
-//   config => {
-//     const token = sessionStorage.getItem('token');
-//     if(token){
-//       config.headers['Authorization'] = `Bearer ${token}`;
-//     }   
-//         return config;
-//     }
-// );
-
  export default instance;
--- SD-545(React)/w1d3_react-typescript/my-app/src/App.tsx
+++ SD-545(React)/w1d3_react-typescript/my-app/src/App.tsx
@@ -1,7 +1,7 @@
 //2. Create a temperature converter component that allows users to enter a temperature in Celsius and 
 //converts it to Fahrenheit when a button is clicked. Use useState to manage the temperature input and output.
 
-import { ChangeEvent, useState } from "react";
+
 
 // function App(){
 
@@ -28,30 +28,73 @@ import { ChangeEvent, useState } from "react";
 
 
 // }
+// function App(){
+//     const [input1,setinput1] = useState(0)
+//     const [input2,setinput2] = useState(0)
+//     const [input3,setinput3] = useState(0)
+//     const change_input1=(e:ChangeEvent<HTMLInputElement>)=>{
+//         setinput1(parseInt(e.currentTarget.value))
+//     }
+//     const change_input2=(e:ChangeEvent<HTMLInputElement>)=>{
+//         setinput2(parseInt(e.currentTarget.value))
+//     }
+//     const chnage_data=()=>{
+//       const addition=input1+input2;
+//       setinput3(addition)
+//     }
+//     return(
+//         <>
+//         <input type="number" value={input1} onChange={change_input1}></input>
+//        + <input type="number" value={input2} onChange={change_input2}></input>
+//        = <input type="number" value={input3} ></input>
+//        <button onClick={chnage_data}>add</button>
+
+//         </>
+//     )
+// }
+
+
+
+// export default App;
+
+
+import { useState } from "react";
+
+
 function App(){
-    const [input1,setinput1] = useState(0)
-    const [input2,setinput2] = useState(0)
-    const [input3,setinput3] = useState(0)
-    const change_input1=(e:ChangeEvent<HTMLInputElement>)=>{
-        setinput1(parseInt(e.currentTarget.value))
-    }
-    const change_input2=(e:ChangeEvent<HTMLInputElement>)=>{
-        setinput2(parseInt(e.currentTarget.value))
-    }
-    const chnage_data=()=>{
-      const addition=input1+input2;
-      setinput3(addition)
-    }
-    return(
-        <>
-        <input type="number" value={input1} onChange={change_input1}></input>
-       + <input type="number" value={input2} onChange={change_input2}></input>
-       = <input type="number" value={input3} ></input>
-       <button onClick={chnage_data}>add</button>
 
-        </>
-    )
+    let[count,setcount] = useState<number>(0)
+
+    const Counter_increment=()=>{
+
+        setcount(count+1)
+        console.log(count)
+        
+    }
+    const Count_decrement=()=>{
+
+       setcount(count-1)
+       console.log(count)
+
+    }
+
+return(
+
+<div>
+    <span>This is react state:</span>
+    {count}
+    <hr></hr>
+
+<button onClick={Counter_increment}>Counter increment</button>
+<hr></hr>
+
+<button onClick={Count_decrement}>Counter decrement</button>
+</div>
+
+    
+)
+
+
 }
-
 
 export default App;
